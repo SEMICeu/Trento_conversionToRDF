1e2f92b2e93831a5a35d5a340367aced6546002b
emidiostani <emidiostani@gmail.com>
2019-04-09 22:07:07 +0200

    added convertion to json-ld

---
+++ transform.py
@@ -0,0 +1,14 @@
+from rdflib.graph import Graph
+from urllib.request import urlopen
+import glob
+import simplejson as json
+listfiles = (glob.glob("./downloads/*.rdf"))
+print(listfiles)
+g = Graph()
+for filename in listfiles:
+	g.parse(file=open(filename, "r"), format="application/rdf+xml", publicID="http://www.provincia.tn.it/")
+	print(len(g))
+
+context_jsonld =  "https://raw.githubusercontent.com/catalogue-of-services-isa/CPSV-AP/master/releases/2.2.1/CPSV-AP_v2.2.1.jsonld"
+jsonldfile = 'output.jsonld'
+g.serialize(destination=jsonldfile, format='json-ld', context=context_jsonld, indent=4, auto_compact=True)
